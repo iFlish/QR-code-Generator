4c90ee0d737b179fbb3a45ebfd660796711d21de
IFlish <ammirruladam@gmail.com>
2025-08-12 21:20:35 +0800

    Delete QR Generator directory

--- QR Generator/script.js
+++
@@ -1,8 +0,0 @@
-let imgBox = document.querySelector(".imgBox"); 
-let qrImage = document.getElementById("qrImage");
-let urlInput = document.getElementById("urlInput");
-
-function generateQR(){
-qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" 
-                              + encodeURIComponent(urlInput.value);
-}
